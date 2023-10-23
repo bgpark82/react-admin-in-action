f76b7f2a8f41146e02602c5a6e88c3fb8547015d
musinsa-bgpark <peter.park@musinsa.com>
2023-10-23 17:30:29 +0900

    feat: posts 데이터 참조
    - Resource.recordRepresentation: reference된 컬럼에서 보여줄 데이터
    - ReferenceField.source: reference할 필드
    - ReferenceField.reference: 참조할 데이터(Resource.name과 동일)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import {Admin, Resource,} from "react-admin";
+import {Admin, ListGuesser, Resource,} from "react-admin";
 import {dataProvider} from "./dataProvider";
 import {UserList} from "./users";
+import {PostList} from "./posts";
 
+/**
+ * Resource: API 호출
+ * name: /users, /posts 등 path
+ * recordRepresentation: reference된 컬럼에서 보여질 데이터
+ */
 export const App = () => <Admin dataProvider={dataProvider}>
-  <Resource name="users" list={UserList} />
+  {/* /posts 엔드포인트로 리소스 조회*/}
+  <Resource name="posts" list={PostList} />
+  <Resource name="users" list={UserList} recordRepresentation="name" />
 </Admin>;
---
+++ src/posts.tsx
@@ -0,0 +1,24 @@
+// in src/posts.tsx
+import {
+    List,
+    Datagrid,
+    TextField,
+    ReferenceField,
+    EditButton
+} from "react-admin";
+
+/**
+ * ReferenceField: reference 데이터를 API로 fetch
+ * reference: <Resource>의 name 필드
+ * @constructor
+ */
+export const PostList = () => (
+    <List>
+        <Datagrid>
+            <TextField source="id" />
+            <ReferenceField source="userId" reference="users" />
+            <TextField source="title" />
+            <EditButton />
+        </Datagrid>
+    </List>
+);
