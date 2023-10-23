283dedf56475aa91ae4f94ad06d07316694faf26
musinsa-bgpark <peter.park@musinsa.com>
2023-10-23 17:04:54 +0900

    feat: List 추가
    - Datagrid : API 응답을 읽어 RecordContext를 생성
    - SimpleList : 간단한 형식의 List 생성

---
+++ src/users.tsx
@@ -0,0 +1,68 @@
+import {Datagrid, EmailField, List, SimpleList, TextField} from 'react-admin';
+import {useMediaQuery} from "@mui/material";
+import MyUrlField from "./MyUrlField";
+
+/**
+ * API로 조회한 쿼리 파라미터를 읽음
+ *
+ * List : fetch data
+ * - Datagrid : rendering
+ * - Datagrid는 RecordContext를 생성한다 (useRecordContext: record 읽음)
+ * SimpleList : fetch data
+ * - Material UI의 List와 ListItem 사용
+ */
+export const UserList = () => {
+    const isSmall = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+
+    return (
+        <List>
+            {isSmall ?
+                (
+                    <SimpleList
+                        primaryText={(record) => record.name}
+                        secondaryText={(record) => record.username}
+                        tertiaryText={(record) => record.email}
+                    >
+                    </SimpleList>
+                ) : (
+                    <Datagrid rowClick="edit">
+                        <TextField source="id" />
+                        <TextField source="name" />
+                        <TextField source="username" />
+                        <EmailField source="email" />
+                        <TextField source="address.street" />
+                        <TextField source="phone" />
+                        <MyUrlField source="website" />
+                        <TextField source="company.name" />
+                    </Datagrid>
+                )
+
+            }
+        </List>
+    )
+};
+/**
+ * {
+ *   "id": 2,
+ *   "name": "Ervin Howell",
+ *   "username": "Antonette",
+ *   "email": "Shanna@melissa.tv",
+ *   "address": {
+ *     "street": "Victor Plains",
+ *     "suite": "Suite 879",
+ *     "city": "Wisokyburgh",
+ *     "zipcode": "90566-7771",
+ *     "geo": {
+ *       "lat": "-43.9509",
+ *       "lng": "-34.4618"
+ *     }
+ *   },
+ *   "phone": "010-692-6593 x09125",
+ *   "website": "anastasia.net",
+ *   "company": {
+ *     "name": "Deckow-Crist",
+ *     "catchPhrase": "Proactive didactic contingency",
+ *     "bs": "synergize scalable supply-chains"
+ *   }
+ * }
+ */
